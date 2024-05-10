118d593cedba2a9f5747c1115cedd2c782c6c7d7
SirDaechel <anayookpala26@gmail.com>
2024-05-10 10:21:21 +0100

    jobs filter functionality completed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,16 +4,20 @@ import Hero from "@/components/Hero";
 import FeaturedJobs from "@/components/FeaturedJobs";
 import SignUpBanner from "@/components/SignUpBanner";
 import LatestJobs from "@/components/LatestJobs";
+import { Suspense } from "react";
+import Loader from "@/components/Loader";
 
 export default async function Home() {
   return (
-    <section className="w-full">
-      <Hero />
-      <CompaniesHelped />
-      <FeaturedCategories />
-      <SignUpBanner />
-      <FeaturedJobs />
-      <LatestJobs />
-    </section>
+    <Suspense fallback={<Loader className="loader" />}>
+      <section className="w-full">
+        <Hero />
+        <CompaniesHelped />
+        <FeaturedCategories />
+        <SignUpBanner />
+        <FeaturedJobs />
+        <LatestJobs />
+      </section>
+    </Suspense>
   );
 }
